07b48ff7a8fc9098cd898b1fd961b074fb5ee9cb
shane garrity <shanedgarrity@gmail.com>
2019-07-12 23:40:11 -0400

    prefer typed struct to tuple primitive

--- src/main.rs
+++ src/main.rs
@@ -6,18 +6,23 @@ fn main() {
 
     let config = parse_config(&args);
 
-    println!("Searching for {}", config.0);
-    println!("In file {}", config.1);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
 
-    let contents = fs::read_to_string(config.1)
+    let contents = fs::read_to_string(config.filename)
         .expect("Something went wrong reading the file");
 
     println!("With text:\n{}", contents);
 }
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let filename = &args[2];
+struct Config {
+    query: String,
+    filename: String,
+}
 
-    (query, filename)
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let filename = args[2].clone();
+
+    Config { query, filename }
 }
