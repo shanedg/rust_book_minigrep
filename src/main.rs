4ddf48c1dd87d47aee866fea6b137bdfd6dafa93
shane garrity <shanedgarrity@gmail.com>
2019-07-12 23:36:46 -0400

    extract argument parsing logic

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,20 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
+    let config = parse_config(&args);
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    println!("Searching for {}", config.0);
+    println!("In file {}", config.1);
 
-    let contents = fs::read_to_string(filename)
+    let contents = fs::read_to_string(config.1)
         .expect("Something went wrong reading the file");
 
     println!("With text:\n{}", contents);
+}
+
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let filename = &args[2];
+
+    (query, filename)
 }
